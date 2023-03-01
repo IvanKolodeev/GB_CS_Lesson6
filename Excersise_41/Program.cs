3b87f6ea58ba5a96700053cca2c13fb7599ed068
Ivan K <ikolodeev002@gmail.com>
2023-03-01 12:30:11 +0200

    Homework

---
+++ Excersise_41/Program.cs
@@ -0,0 +1,37 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    Console.Write(array[i] + "  ");
+}
+
+int[] StringToArray(string numbers)
+{
+    string[] str = numbers.Split(" ", StringSplitOptions.None);
+    int[] result = new int[str.Length];
+
+    for (int i = 0; i<str.Length; i++)
+    {
+        result[i] = int.Parse(str[i]);
+    }
+
+    return result;
+}
+
+Console.Write("Задайте элементы массива через пробел: ");
+string elements = Console.ReadLine();
+int[] array = StringToArray(elements);
+PrintArray(array);
+
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    count = array[i]>0 ? count+1 : count;
+}
+
+Console.WriteLine();
+Console.WriteLine($"Count of positive numbers is {count}");
---
+++ Excersise_43/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1 
+// y = k2 * x + b2 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Задайте элементы для уравнения y = k1 * x + b1");
+Console.Write("k1 = ");
+double k1 = double.Parse(Console.ReadLine()!);
+Console.Write("b1 = ");
+double b1 = double.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Задайте элементы для уравнения y = k2 * x + b2");
+Console.Write("k2 = ");
+double k2 = double.Parse(Console.ReadLine()!);
+Console.Write("b2 = ");
+double b2 = double.Parse(Console.ReadLine()!);
+
+if (k1 == k2)
+{
+    // Handle the case where k1 and k2 are equal separately
+    Console.WriteLine("Error: k1 and k2 are equal, cannot solve system.");
+}
+else
+{
+    double x = (b2-b1) / (k1-k2);
+    double y = k2 * x + b2;
+    Console.WriteLine($"({x}; {y})");
+}
